900a8df73971d9cdf429ebeffadad3844ec5dc69
jperedo <peredo01@gmail.com>
2024-04-05 11:01:08 -0500

    Added LLama2 model name

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,6 +1,7 @@
 from typing import Final
 
 GROQ_MIXTRAL_MODEL_NAME: Final[str] = "mixtral-8x7b-32768"
+GROQ_LLAMA2_MODEL_NAME: Final[str] = "llama2-70b-4096"
 
 OPENAI_GPT35_MODEL_NAME: Final[str] = "gpt-3.5-turbo"
 OPENAI_GPT4_MODEL_NAME: Final[str] = "gpt-4"
